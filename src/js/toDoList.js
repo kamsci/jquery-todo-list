065518c78fb282c9797d72d7808b051de22cb71e
Krista Calderon <kamsci@gmail.com>
2016-05-31 22:20:34 -0700

    updated css; added trim function

--- src/js/toDoList.js
+++ src/js/toDoList.js
@@ -1,29 +1,18 @@
 $(document).ready(function() {
-
-// user adds item to list
-  // get input from user
-$('#submit').click(function(e) {
-  e.preventDefault();
-  var item = $('#addItem').val();
-  $('ul').first().append('<li>' + item + '<button class="btn">X</button' + '</li>');
-  $('#addItem').val('');
-  $('#addItem').focus();
-});
-  // add new list item to html
-  // add 'delete' box next to item
-
+// get input from user
+// add new list item to html
+// clear and refocus after each submit
+// add 'delete' box next to item
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    var item = $('#addItem').val().trim();
+    if (item !=='') {
+      $('ul').first().append('<li>' + item + '<button class="btn">X</button' + '</li>');
+      $('#addItem').val('');
+      $('#addItem').focus();
+    }
+  });
   $('ul').delegate('li', 'click', function() {
     $(this).remove();
   });
-
-
-// $('div').delegate('li button', function (
-
-// user removes item from list
-  // click x to delete
-  // end of doc ready
-
-// clear and refocus after each submit
-
-
-});
+}); // end of doc ready
